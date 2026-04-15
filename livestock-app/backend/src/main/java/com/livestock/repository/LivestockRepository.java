7c9c64c04c86f8003dd9417021669c11d54dc494
kishore <gorlakishore9@gmail.com>
2026-04-15 04:55:34 +0000

    update psql

--- livestock-app/backend/src/main/java/com/livestock/repository/LivestockRepository.java
+++ livestock-app/backend/src/main/java/com/livestock/repository/LivestockRepository.java
@@ -2,6 +2,8 @@ package com.livestock.repository;
 
 import com.livestock.model.Livestock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LivestockRepository extends JpaRepository<Livestock, Long> {
 }
